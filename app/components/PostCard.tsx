1554c9b341098428d383cfa3e1642bfe7e52e185
Sevag <baghsev@gmail.com>
2024-11-04 11:44:17 -0500

    Add comments count to post cards

--- app/components/PostCard.tsx
+++ app/components/PostCard.tsx
@@ -10,28 +10,40 @@ interface PostCardProps {
     programmingLanguages: string[];
     authorImage: string;
     createdAt: Date;
+    commentIds: string[];
   };
 }
 
 export default function PostCard({ post }: PostCardProps) {
   return (
     <div className="transition hover:shadow-xl flex flex-col bg-white my-4 rounded-sm p-4 gap-2">
-      <p>
-        <span className="font-bold">{post.title}</span>
-      </p>
-      <div className="flex items-center gap-2">
+      <div className="flex flex-col justify-start sm:flex-row  sm:justify-between">
         <p>
-          posted by <span className="font-bold">{post.authorUsername}</span>
+          <span className="font-bold">{post.title}</span>
         </p>
+        <p>
+          {post.commentIds.length === 0 ? "No" : post.commentIds.length}{" "}
+          {post.commentIds.length === 1 ? "comment" : "comments"}
+        </p>
+      </div>
 
-        <Image
-          src={post.authorImage}
-          alt="profile picture"
-          width={32}
-          height={32}
-          className="rounded-full"
-        />
-        <p>{moment(post.createdAt).startOf("hour").fromNow()}</p>
+      <div className="flex flex-col sm:flex-row sm:items-center gap-2">
+        <div className="flex items-center gap-2">
+          <p>
+            posted by <span className="font-bold">{post.authorUsername}</span>
+          </p>
+
+          <Image
+            src={post.authorImage}
+            alt="profile picture"
+            width={32}
+            height={32}
+            className="rounded-full"
+          />
+        </div>
+        <div>
+          <p>{moment(post.createdAt).startOf("hour").fromNow()}</p>
+        </div>
       </div>
       <hr />
       <p>
